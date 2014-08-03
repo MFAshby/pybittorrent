d8ab36a32a4a1815c566b07c5f8fadc486683220
Martin Ashby <martin@ashbysoft.com>
2014-08-03 23:32:34 +0100

    finished .torrent file creator

--- Bittorrent/metainfo_create.py
+++ Bittorrent/metainfo_create.py
@@ -1,38 +1,62 @@
 from os.path import getsize
 from hashlib import sha1
 from bencode import bencode
+from time import time
 
-PIECE_LENGTH = 512
-
-def create(output_filename="",
-           filename="", 
+def create(output_file="",
+           input_file=None, 
+           input_file_length=0,
+           input_file_name="",
+           piece_length=512000,
+           pieces_encoding="iso-8859-15",
            announce_url="http://localhost:8000", 
            comment="Test torrent file", 
-           created_by="",
-           encoding="UTF-8"):
-    """takes a file, creates a metainfo file ready for torrenting"""
-    length = getsize(filename)
-    pieces = b""
+           created_by=""):
+    """takes an input file, creates a metainfo file ready for torrenting"""
+    pieces = bytearray()
+    file_piece = input_file.read(piece_length)
+    while file_piece:
+        the_hash = sha1()
+        the_hash.update(file_piece)
+        pieces += the_hash.digest()
+        file_piece = input_file.read(piece_length)
 
-    with open(filename, "rb") as f:
-        file_piece = f.read(PIECE_LENGTH)
-        while file_piece:
-            the_hash = sha1()
-            the_hash.update(file_piece)
-            pieces += the_hash.digest()
-            file_piece = f.read(PIECE_LENGTH)
-
-    
     to_write = {"info": 
-                  {"piece length": PIECE_LENGTH,
-                    "name": filename,
-                    "length": length,
-                    "pieces": str(pieces, encoding)},
+                  {"piece length": piece_length,
+                    "name": input_file_name,
+                    "length": input_file_length,
+                    "pieces": pieces.decode(encoding=pieces_encoding)},
                 "announce":announce_url,
-                "creation date": 1000000000,
+                "creation date": int(time()),
                 "comment": comment,
                 "created by": created_by,
-                "encoding": encoding}
+                "encoding": pieces_encoding}
+    to_write_str = bencode(to_write)
+    output_file.write(to_write_str)
 
-    with open(output_filename, "w") as f:
-        f.write(bencode(to_write))
+if __name__ == "__main__":
+    import argparse
+    from os.path import getsize
+    parser = argparse.ArgumentParser(description='Create a metainfo file (.torrent)')
+    parser.add_argument('INFILE', type=str, help='the file you want to seed')
+    parser.add_argument('OUTFILE', type=str, help='output (.torrent) file')
+    parser.add_argument('URL', type=str, help='URL where the tracker is based')
+    parser.add_argument('-c', dest="comment", type=str, default="", help='File description')
+    parser.add_argument('-w', dest="created_by", type=str, default="", help='Who created this torrent')
+    parser.add_argument('-l', dest="piece_length", type=int, default=512000, help='length of pieces (default to 512kB')
+    parser.add_argument('-e', dest="piece_encoding", type=str, default="iso-8859-15", help='string encoding for the pieces hash (default iso-8859-15)')
+
+    args = parser.parse_args()
+    with open(args.INFILE, "rb") as in_file, open(args.OUTFILE, "w") as out_file:
+        in_file_size = getsize(args.INFILE)
+        create(output_file=out_file,
+               input_file=in_file, 
+               input_file_length=in_file_size,
+               input_file_name=args.INFILE,
+               piece_length=args.piece_length,
+               pieces_encoding=args.piece_encoding,
+               announce_url=args.URL, 
+               comment=args.comment, 
+               created_by=args.created_by)
+
+
--- Bittorrent/metainfo_create_tests.py
+++ Bittorrent/metainfo_create_tests.py
@@ -1,22 +1,42 @@
 from unittest import TestCase
 import unittest
 from metainfo_create import create
-from os.path import getsize
+from io import BytesIO, StringIO
+from bencode import bdecode
 
 class MetainfoTestCase(TestCase):
     def test_create(self):
-        #write a test file... 
-        with open("test.file", "w") as f:
-            f.write("Testing torrent file!!!")
-            for i in range(100000):
-                f.write(str(i))
+        #mock file-like objects to read and write
+        mock_output = StringIO()
+        test_bytes = b"Testing file!"
+        test_bytes_length = len(test_bytes)
+        mock_input = BytesIO(test_bytes)
+        
+        create(output_file=mock_output,
+           input_file=mock_input, 
+           input_file_length=test_bytes_length,
+           input_file_name="Testing.file",
+           piece_length=512000,
+           pieces_encoding="iso-8859-15",
+           announce_url="http://localhost:8000", 
+           comment="Test torrent file", 
+           created_by="Martin Ashby's Test script...")
 
-        create(output_filename="test.torrent",
-               filename="test.file", 
-               announce_url="http://localhost:8000", 
-               comment="Test torrent file", 
-               created_by="Martin Ashby",
-               encoding="iso-8859-15")
+        mock_output.seek(0)
+        output_dict = bdecode(mock_output.read())
+
+        #check the output has the appropriate keys.
+        info_dict = output_dict.get("info")
+        self.assertTrue(len(info_dict))
+        self.assertTrue(info_dict.get("piece length") == 512000)
+        self.assertTrue(info_dict.get("name") == "Testing.file")
+        self.assertTrue(info_dict.get("length") == test_bytes_length)
+        self.assertTrue(info_dict.get("pieces"))
+        self.assertTrue(output_dict.get("announce") == "http://localhost:8000")
+        self.assertTrue(type(output_dict.get("creation date")) == int)
+        self.assertTrue(output_dict.get("comment") == "Test torrent file")
+        self.assertTrue(output_dict.get("created by") == "Martin Ashby's Test script...")
+        self.assertTrue(output_dict.get("encoding") == "iso-8859-15")
 
 if __name__ == "__main__":
     unittest.main()
