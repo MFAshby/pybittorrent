9b596337cc7f869c1a3e84bb64829e53b042c042
Martin Ashby <martin@ashbysoft.com>
2014-08-05 00:54:20 +0100

    adding bits of client

--- Bittorrent/bittorrent.py
+++ Bittorrent/bittorrent.py
@@ -1,13 +1,38 @@
 #basic bittorrent client. Start off with single-file torrents, 
+from bencode import bdecode
 
-#peers, by IP address
-peers = {}
+#index of pieces and whether we have them. (have_pieces[0] == 1) 
+#if we have the first piece. have_pieces[123] == 0 if we don't 
+#have the 124th piece
+have_pieces = []
 
-class Peer:
-    am_choking = True
-    am_interested = False
-    peer_choking = True
-    peer_interested = False
+def do_handshake(output_socket, info_hash, peer_id):
+    output_socket.write(b"\x13")
+    output_socket.write(bytes("BitTorrent protocol", "UTF-8"))
+    output_socket.write(b'\x00'*8)
+    output_socket.write(bytes(info_hash, "UTF-8"))
+    output_socket.write(bytes(peer_id, "UTF-8"))
 
-if __name__ == "__main__":
+def get_peer_id():
+    import random
+    import string
+    max_char = string.ascii_uppercase + string.digits
+    return "-PY0001-" + ''.join(random.choice(max_char) for _ in range(12))
     
+def do_keep_alive(output_socket):
+    output_socket.write(b"\x00")
+
+def do_choke(output_socket):
+    output_socket.write(b"\x01\x00")
+
+def do_unchoke(output_socket):
+    output_socket.write(b"\x01\x01")
+
+def do_interested(output_socket):
+    output_socket.write(b"\x01\x02")
+
+def do_not_interested(output_socket):
+    output_socket.write(b"\x01\x03")
+
+def do_have(output_socket, piece_index):
+    output_socket.write(b"\x05\x04")
--- Bittorrent/bittorrent_tests.py
+++ Bittorrent/bittorrent_tests.py
@@ -1,1 +1,78 @@
-#nothing yet..
+from unittest import TestCase, main
+from io import BytesIO
+import bittorrent
+
+
+class BittorrentTest(TestCase):
+    def test_handshake(self): 
+        info_hash = "info4567890123456789"
+        peer_id = "peer4567890123456789"
+        output = self.do_test_fn_return_output(bittorrent.do_handshake, info_hash, peer_id)
+        #58 bytes total output
+        self.assertTrue(len(output) == 68)
+        #first byte is protocol string length, should be 19
+        self.assertTrue(output[0] == 19)
+        #next 19 bytes are the protocol string
+        self.assertTrue(output[1:20].decode("UTF-8") == "BitTorrent protocol")
+        #8 empty bytes
+        self.assertTrue(output[20:28] == b'\x00'*8)
+        #20 byte info hash
+        self.assertTrue(output[28:48].decode("UTF-8") == info_hash)
+        #20 byte peer id
+        self.assertTrue(output[48:68].decode("UTF-8") == peer_id)
+
+    def test_peer_id(self):
+        peer_id = bittorrent.get_peer_id()
+        self.assertTrue(len(peer_id) == 20)
+        self.assertTrue(peer_id.startswith("-PY0001-"))
+        #subsequent call returns a different peer_id
+        peer_id_2 = bittorrent.get_peer_id()
+        self.assertTrue(len(peer_id_2) == 20)
+        self.assertTrue(peer_id_2.startswith("-PY0001-"))
+        self.assertTrue(peer_id != peer_id_2)
+
+    def test_keep_alive(self):
+        output = self.do_test_fn_return_output(bittorrent.do_keep_alive)
+        self.assertTrue(len(output) == 1)
+        self.assertTrue(output[0] == 0)
+
+    def test_choke(self):
+        output = self.do_test_fn_return_output(bittorrent.do_choke)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(output[0] == 1) #length 1
+        self.assertTrue(output[1] == 0) #message ID 0
+
+    def test_unchoke(self):
+        output = self.do_test_fn_return_output(bittorrent.do_unchoke)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(output[0] == 1) #length 1
+        self.assertTrue(output[1] == 1) #message ID 1
+
+    def test_interested(self):
+        output = self.do_test_fn_return_output(bittorrent.do_interested)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(output[0] == 1) #length 1
+        self.assertTrue(output[1] == 2) #message ID 2
+
+    def test_not_interested(self):
+        output = self.do_test_fn_return_output(bittorrent.do_not_interested)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(output[0] == 1) #length 1
+        self.assertTrue(output[1] == 3) #message ID 3
+
+    def test_have(self):
+        output = self.do_test_fn_return_output(bittorrent.do_have, 1)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(output[0] == 5) #length 5
+        self.assertTrue(output[1] == 4) #message ID 4
+        self.assertTrue(output[1:4] == b"\x00\x00\x00\x01")
+
+    def do_test_fn_return_output(self, function, *args):
+        output_socket = BytesIO()
+        function(output_socket, *args)
+        output_socket.seek(0)
+        return output_socket.read()
+
+
+if __name__ == "__main__":
+    main()
